da55023caefcb8db75757ef93113dc1200887ca3
Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>
2018-05-24 10:49:10 +0530

    Basic support for User manager service
    Basic support for User Manager service methods
    are implemented.
    
    Change-Id: Id42432ec6dd421b99971268add931dcd70876f7c
    Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

--- mainapp.cpp
+++ mainapp.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 #include <string>
-#include "user.hpp"
+#include "user_mgr.hpp"
 #include "config.h"
 
 // D-Bus root for user manager
@@ -23,22 +23,15 @@ constexpr auto USER_MANAGER_ROOT = "/xyz/openbmc_project/user";
 int main(int argc, char** argv)
 {
     auto bus = sdbusplus::bus::new_default();
-
-    // This is hard coded "root" user.
-    // TODO: This would need to be changed when the complete
-    //       user management code is written. May be, have manager
-    //       create these user objects.
-    //       Issue: openbmc/openbmc#2299
-    auto objPath = std::string{USER_MANAGER_ROOT} + '/' + "root";
-
     sdbusplus::server::manager::manager objManager(bus, USER_MANAGER_ROOT);
-    phosphor::user::User user(bus, objPath.c_str());
+
+    phosphor::user::UserMgr userMgr(bus, USER_MANAGER_ROOT);
 
     // Claim the bus now
     bus.request_name(USER_MANAGER_BUSNAME);
 
     // Wait for client request
-    while(true)
+    while (true)
     {
         // process dbus calls / signals discarding unhandled
         bus.process_discard();
