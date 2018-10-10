a2bcc90a76fe90507df6327d95030245b54357c8
Nagaraju Goruganti <ngorugan@in.ibm.com>
2018-10-10 00:55:27 -0500

    phosphor-ldap-conf: update nslcd.conf file with tls_cacertfile info
    tls_cacertfile specifies the path to the X.509 certificate for
    peer authentication.
    
    Also updated the file with "tls_reqcert hard", to force the
    behavior: if no certificate is provided, or a bad certificate
    is provided, the session is immediately terminated.
    
    Tested: tested using below given commands
    1.curl -c cjar -b cjar -k -H "Content-Type: application/json" -X  POST -d \
      '{"data":[true,"ldaps://9.194.251.139/","cn=Sivas,dc=Corp,dc=ibm,dc=com",\
      "cn=Users,dc=Corp,dc=ibm,dc=com",  "Welcome3ibm!",\
      "xyz.openbmc_project.User.Ldap.Create.SearchScope.sub",\
      "xyz.openbmc_project.User.Ldap.Create.Type.ActiveDirectory"] \
    }' https://$BMC_IP//xyz/openbmc_project/user/ldap/action/CreateConfig
    
    2.curl -b cjar -k -H "Content-Type: application/json" -X PUT -d '{"data":true}'\
      https://$BMC_IP/xyz/openbmc_project/user/ldap/config/attr/SecureLDAP
    
    3.curl -b cjar -k -H "Content-Type: application/json" -X PUT -d \
      '{"data":"ldap://9.194.251.136/"}' \
      https://$BMC_IP/xyz/openbmc_project/ldap/config/attr/LDAPServerURI
    
    when "/etc/ssl/certs/Root-CA.pem" doesn't exist on target, we get below
    given error(if we try to set SecureLDAP is true):
    
    root@witherspoon-w5:~# Oct 17 08:03:04 witherspoon-w5 phosphor-ldap-conf[1799]:
    LDAP server's CA certificate not provided
    Oct 17 08:03:04 witherspoon-w5 phosphor-ldap-conf[1799]: The operation is not
    allowed
    ...........................
    ..........................
    Oct 17 08:03:04 witherspoon-w5 phosphor-ldap-conf[1799]:
    xyz.openbmc_project.Common.Error.NotAllowed: The operation is not allowed
    Oct 17 08:03:04 witherspoon-w5 phosphor-ldap-conf[1799]: The operation failed
    internally.
    
    when we try to set serverURI without the prefix "ldaps"
    (ex:"ldap://9.194.251.139/), we get below given error(if we try to set
    SecureLDAP is true):
    
    Oct 17 08:10:24 witherspoon-w5 phosphor-ldap-conf[1799]: bad LDAPSURI
    Oct 17 08:10:24 witherspoon-w5 phosphor-ldap-conf[1799]: Invalid argument was
    given.
    .............................
    .............................
    Oct 17 08:10:24 witherspoon-w5 phosphor-gevent[1262]:
    DBusException: xyz.openbmc_project.Common.Error.InvalidArgument:
    Invalid argument was given.
    
    Change-Id: I56ffe8b08bb71307b4f2bfe9cf935b6113e4579a
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- phosphor-ldap-config/ldap_configuration.cpp
+++ phosphor-ldap-config/ldap_configuration.cpp
@@ -10,11 +10,13 @@ namespace ldap
 {
 constexpr auto nslcdService = "nslcd.service";
 constexpr auto nscdService = "nscd.service";
+constexpr auto tlsCacertfile = "/etc/ssl/certs/Root-CA.pem";
 
 using namespace phosphor::logging;
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 namespace fs = std::experimental::filesystem;
 using Argument = xyz::openbmc_project::Common::InvalidArgument;
+using NotAllowedArg = xyz::openbmc_project::Common::NotAllowed;
 
 using Line = std::string;
 using Key = std::string;
@@ -104,8 +106,8 @@ void Config::writeConfig()
     if (secureLDAP() == true)
     {
         confData << "ssl on\n";
-        confData << "tls_reqcert allow\n";
-        confData << "tls_cert /etc/nslcd/certs/cert.pem\n";
+        confData << "tls_reqcert hard\n";
+        confData << "tls_cacertfile " << tlsCacertfile << "\n";
     }
     else
     {
@@ -176,12 +178,22 @@ bool Config::secureLDAP(bool value)
         {
             return value;
         }
-
+        if (value && !fs::exists(tlsCacertfile))
+        {
+            log<level::ERR>("LDAP server's CA certificate not provided",
+                            entry("TLSCACERTFILE=%s", tlsCacertfile));
+            elog<NotAllowed>(NotAllowedArg::REASON(
+                "LDAP server's CA certificate not provided"));
+        }
         val = ConfigIface::secureLDAP(value);
         writeConfig();
         parent.restartService(nslcdService);
     }
     catch (const InternalFailure& e)
+    {
+        throw;
+    }
+    catch (const NotAllowed& e)
     {
         throw;
     }
@@ -421,6 +433,13 @@ std::string
                             ldap_base::Create::SearchScope lDAPSearchScope,
                             ldap_base::Create::Type lDAPType)
 {
+    if (secureLDAP && !fs::exists(tlsCacertfile))
+    {
+        log<level::ERR>("LDAP server's CA certificate not provided",
+                        entry("TLSCACERTFILE=%s", tlsCacertfile));
+        elog<NotAllowed>(
+            NotAllowedArg::REASON("LDAP server's CA certificate not provided"));
+    }
     if (!(ldap_is_ldap_url(lDAPServerURI.c_str()) ||
           ldap_is_ldaps_url(lDAPServerURI.c_str())))
     {
@@ -595,6 +614,12 @@ void ConfigMgr::restore(const char* filePath)
         // Don't throw - we don't want to create a D-Bus
         // object upon finding empty values in config, as
         // this can be a default config.
+    }
+    catch (const NotAllowed& e)
+    {
+        // Don't throw - we don't want to create a D-Bus
+        // object upon finding "ssl on" without having tls_cacertfile in place,
+        // as this can be a default config.
     }
     catch (const InternalFailure& e)
     {
