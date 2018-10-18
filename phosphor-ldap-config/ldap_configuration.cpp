3414e7ad68b396d20dea21204298371dbc308819
Ratan Gupta <ratagupt@in.ibm.com>
2018-10-18 09:57:14 +0530

    phosphor-ldap-conf: Make correction in renaming path of nsswitch.conf
    By default nscd comes with nsswitch.conf, we had one more file for the
    ldap specific version, and we copy the content from the ldap nsswitch
    to the nsswitch.conf once LDAP config object gets created/deleted.
    
    We had some inconsistency during restarting of services so thought of
    clean logic where we would be having two files nsswitch_linux/nsswitch_ldap
    and when ldap config object gets created we copy the nsswitch_ldap to
    nsswitch.conf and when it gets deleted then  copy the nsswitch_linux
    to nsswitch.conf
    
    Change-Id: I5a0af3ec82dd08fc54c7423fda1a80509769872d
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- phosphor-ldap-config/ldap_configuration.cpp
+++ phosphor-ldap-config/ldap_configuration.cpp
@@ -49,8 +49,7 @@ void Config::delete_()
     {
         fs::copy_file(defaultNslcdFile, LDAP_CONFIG_FILE,
                       fs::copy_options::overwrite_existing);
-        fs::copy_file(nsSwitchFile, LDAPNsSwitchFile,
-                      fs::copy_options::overwrite_existing);
+
         fs::copy_file(linuxNsSwitchFile, nsSwitchFile,
                       fs::copy_options::overwrite_existing);
     }
@@ -414,8 +413,6 @@ std::string
     deleteObject();
     try
     {
-        fs::copy_file(nsSwitchFile, linuxNsSwitchFile,
-                      fs::copy_options::overwrite_existing);
         fs::copy_file(LDAPNsSwitchFile, nsSwitchFile,
                       fs::copy_options::overwrite_existing);
     }
