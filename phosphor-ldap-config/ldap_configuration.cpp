a6f42d2d5399cd9546d93e9b14d185f2de4de76d
Nagaraju Goruganti <ngorugan@in.ibm.com>
2018-10-23 01:05:13 -0500

    phosphor-ldap-conf: handle "InstallCompleted" signal
    When LDAP client certificate is uploaded, phosphor-certificate-manager
    app would send the signal "InstallCompleted". Upon receiving the
    signal, update the config file with below given info:
         tls_cert <path client certificate file>
         tls_key  <path to client certificate file>
    Tested:
    Below given command adds tls_cert and tls_key related
    entries into nslcd.conf file, when secureLDAP is enabled.
    $curl -c cjar -b cjar -k H "Content-Type: application/octet-stream" -X PUT -T \
        cert.pem  https://$BMC_IP/xyz/openbmc_project/certs/client/ldap
    
    Change-Id: If83bd04612e97c4a6838ba603443583d3a225e3c
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- phosphor-ldap-config/ldap_configuration.cpp
+++ phosphor-ldap-config/ldap_configuration.cpp
@@ -22,7 +22,7 @@ using Val = std::string;
 using ConfigInfo = std::map<Key, Val>;
 
 Config::Config(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
-               const char* caCertfile, bool secureLDAP,
+               const char* caCertfile, const char* certfile, bool secureLDAP,
                std::string lDAPServerURI, std::string lDAPBindDN,
                std::string lDAPBaseDN, std::string&& lDAPBindDNPassword,
                ldap_base::Config::SearchScope lDAPSearchScope,
@@ -48,6 +48,24 @@ Config::Config(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
     parent.restartService(nslcdService);
     // Emit deferred signal.
     this->emit_object_added();
+}
+
+void Config::certificateInstalled(sdbusplus::message::message& msg)
+{
+    try
+    {
+        writeConfig();
+        parent.restartService(nslcdService);
+    }
+    catch (const InternalFailure& e)
+    {
+        throw;
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>(e.what());
+        elog<InternalFailure>();
+    }
 }
 
 void Config::delete_()
@@ -114,6 +132,11 @@ void Config::writeConfig()
         confData << "ssl on\n";
         confData << "tls_reqcert hard\n";
         confData << "tls_cacertfile " << tlsCacertfile.c_str() << "\n";
+        if (fs::exists(tlsCertfile.c_str()))
+        {
+            confData << "tls_cert " << tlsCertfile.c_str() << "\n";
+            confData << "tls_key " << tlsCertfile.c_str() << "\n";
+        }
     }
     else
     {
