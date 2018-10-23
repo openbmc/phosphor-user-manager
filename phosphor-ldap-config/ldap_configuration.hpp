965634429aae80e8b0fb0a10a53d0a90c1f470ae
Nagaraju Goruganti <ngorugan@in.ibm.com>
2018-10-23 01:05:13 -0500

    phosphor-ldap-conf: handle "InstallCompleted" signal
    When LDAP client certificate is uploaded, phosphor-certificate-manager
    app would send the signal "InstallCompleted". Upon receiving the
    signal, update the config file with below given info:
         tls_cert <path client certificate file>
         tls_key  <path to client certificate file>
    Tested:
    Below given command adds tls_cert and tls_key related
    entries into nslcd.conf file, when secureLDAP is enabled.
    $curl -c cjar -b cjar -k H "Content-Type: application/octet-stream" -X PUT -T \
        cert.pem  https://$BMC_IP/xyz/openbmc_project/certs/client/ldap
    
    Change-Id: If83bd04612e97c4a6838ba603443583d3a225e3c
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- phosphor-ldap-config/ldap_configuration.hpp
+++ phosphor-ldap-config/ldap_configuration.hpp
@@ -25,6 +25,7 @@ namespace ldap_base = sdbusplus::xyz::openbmc_project::User::Ldap::server;
 using ConfigIface = sdbusplus::server::object::object<
     ldap_base::Config, sdbusplus::xyz::openbmc_project::Object::server::Delete>;
 using CreateIface = sdbusplus::server::object::object<ldap_base::Create>;
+namespace sdbusRule = sdbusplus::bus::match::rules;
 
 class ConfigMgr;
 
@@ -48,6 +49,7 @@ class Config : public ConfigIface
      *  @param[in] path - The D-Bus object path to attach at.
      *  @param[in] filePath - LDAP configuration file.
      *  @param[in] caCertfile - LDAP's CA certificate file.
+     *  @param[in] certfile - LDAP's client certificate file.
      *  @param[in] secureLDAP - Specifies whether to use SSL or not.
      *  @param[in] lDAPServerURI - LDAP URI of the server.
      *  @param[in] lDAPBindDN - distinguished name with which to bind.
@@ -60,9 +62,9 @@ class Config : public ConfigIface
      */
 
     Config(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
-           const char* caCertfile, bool secureLDAP, std::string lDAPServerURI,
-           std::string lDAPBindDN, std::string lDAPBaseDN,
-           std::string&& lDAPBindDNPassword,
+           const char* caCertfile, const char* certfile, bool secureLDAP,
+           std::string lDAPServerURI, std::string lDAPBindDN,
+           std::string lDAPBaseDN, std::string&& lDAPBindDNPassword,
            ldap_base::Config::SearchScope lDAPSearchScope,
            ldap_base::Config::Type lDAPType, ConfigMgr& parent);
 
@@ -113,6 +115,7 @@ class Config : public ConfigIface
   private:
     std::string configFilePath{};
     std::string tlsCacertfile{};
+    std::string tlsCertfile{};
     std::string lDAPBindDNPassword{};
 
     /** @brief Persistent sdbusplus D-Bus bus connection. */
@@ -124,6 +127,12 @@ class Config : public ConfigIface
 
     /** @brief reference to config manager object */
     ConfigMgr& parent;
+
+    /** @brief React to InstallCompleted signal
+     *  @param[in] msg - sdbusplus message
+     */
+    void certificateInstalled(sdbusplus::message::message& msg);
+    sdbusplus::bus::match_t certificateInstalledSignal;
 };
 
 /** @class ConfigMgr
@@ -146,11 +155,13 @@ class ConfigMgr : public CreateIface
      *  @param[in] path - Path to attach at.
      *  @param[in] filePath - LDAP configuration file.
      *  @param[in] caCertfile - LDAP's CA certificate file.
+     *  @param[in] certfile - LDAP's client certificate file.
      */
     ConfigMgr(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
-              const char* caCertfile) :
+              const char* caCertfile, const char* certfile) :
         CreateIface(bus, path, true),
-        configFilePath(filePath), tlsCacertfile(caCertfile), bus(bus)
+        configFilePath(filePath), tlsCacertfile(caCertfile),
+        tlsCertfile(certfile), bus(bus)
     {
         try
         {
@@ -200,6 +211,7 @@ class ConfigMgr : public CreateIface
   protected:
     std::string configFilePath{};
     std::string tlsCacertfile{};
+    std::string tlsCertfile{};
 
     /** @brief Persistent sdbusplus D-Bus bus connection. */
     sdbusplus::bus::bus& bus;
