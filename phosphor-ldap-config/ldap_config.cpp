f5f088a3c01ff2f219e747e7ef10236095b0a04a
Ratan Gupta <ratagupt@linux.vnet.ibm.com>
2019-04-29 19:39:51 +0530

    phosphor-ldap-conf: handle "PropertiesChanged" signal on the ldap cert object
     When LDAP client certificate is changed through Replace method on the cert object.
     Object would emit the signal "PropertiesChanged". Upon receiving the
     signal, config file would be updated with below given info if
     secure ldap is enabled:
             tls_cert <path client certificate file>
             tls_key  <path to client certificate file>
    
    Tested By: Unit Tested
    
    Signed-off-by: Ratan Gupta <ratagupt@linux.vnet.ibm.com>
    Change-Id: I5347b13e0bf76742d39dc7a26c96ec5d4dd5a2c6

--- phosphor-ldap-config/ldap_config.cpp
+++ phosphor-ldap-config/ldap_config.cpp
@@ -28,6 +28,8 @@ constexpr auto nscdService = "nscd.service";
 constexpr auto LDAPscheme = "ldap";
 constexpr auto LDAPSscheme = "ldaps";
 constexpr auto certObjPath = "/xyz/openbmc_project/certs/client/ldap";
+constexpr auto certIface = "xyz.openbmc_project.Certs.Certificate";
+constexpr auto certProperty = "CertificateString";
 
 using namespace phosphor::logging;
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
@@ -59,6 +61,11 @@ Config::Config(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
     certificateInstalledSignal(
         bus, sdbusplus::bus::match::rules::interfacesAdded(certObjPath),
         std::bind(std::mem_fn(&Config::certificateInstalled), this,
+                  std::placeholders::_1)),
+    certificateChangedSignal(
+        bus,
+        sdbusplus::bus::match::rules::propertiesChanged(certObjPath, certIface),
+        std::bind(std::mem_fn(&Config::certificateChanged), this,
                   std::placeholders::_1))
 {
     ConfigIface::lDAPServerURI(lDAPServerURI);
@@ -105,6 +112,11 @@ Config::Config(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
     certificateInstalledSignal(
         bus, sdbusplus::bus::match::rules::interfacesAdded(certObjPath),
         std::bind(std::mem_fn(&Config::certificateInstalled), this,
+                  std::placeholders::_1)),
+    certificateChangedSignal(
+        bus,
+        sdbusplus::bus::match::rules::propertiesChanged(certObjPath, certIface),
+        std::bind(std::mem_fn(&Config::certificateChanged), this,
                   std::placeholders::_1))
 {
     ConfigIface::lDAPType(lDAPType);
@@ -126,6 +138,29 @@ Config::Config(sdbusplus::bus::bus& bus, const char* path, const char* filePath,
 
 void Config::certificateInstalled(sdbusplus::message::message& msg)
 {
+    try
+    {
+        if (enabled())
+        {
+            writeConfig();
+        }
+        parent.startOrStopService(nslcdService, enabled());
+    }
+    catch (const InternalFailure& e)
+    {
+        throw;
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>(e.what());
+        elog<InternalFailure>();
+    }
+}
+
+void Config::certificateChanged(sdbusplus::message::message& msg)
+{
+    // TODO: Property filtering needs to be done, we need to write
+    // the config only when the property is "Certificate String".
     try
     {
         if (enabled())
