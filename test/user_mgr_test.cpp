978a011e6c2c0014e20b8aa468e65ea2b66b794e
Ravi Teja <raviteja28031990@gmail.com>
2020-08-21 12:46:37 -0500

    check if test case running parallely
    Signed-off-by: Ravi Teja <raviteja28031990@gmail.com>
    Change-Id: I081a01024f1011d72910a0e1e6985e2f3d9eefe4

--- test/user_mgr_test.cpp
+++ test/user_mgr_test.cpp
@@ -3,6 +3,7 @@
 #include <xyz/openbmc_project/Common/error.hpp>
 #include <gtest/gtest.h>
 #include <exception>
+#include <sdbusplus/test/sdbus_mock.hpp>
 
 namespace phosphor
 {
@@ -17,11 +18,12 @@ using InternalFailure =
 class TestUserMgr : public testing::Test
 {
   public:
-    sdbusplus::bus::bus bus;
+    // sdbusplus::bus::bus bus;
+    sdbusplus::SdBusMock sdbusMock;
+    sdbusplus::bus::bus mockedBus = sdbusplus::get_mocked_new(&sdbusMock);
     MockManager mockManager;
 
-    TestUserMgr() :
-        bus(sdbusplus::bus::new_default()), mockManager(bus, objpath)
+    TestUserMgr() : mockManager(mockedBus, objpath)
     {
     }
 
@@ -29,51 +31,19 @@ class TestUserMgr : public testing::Test
                          std::vector<std::string> groupNames,
                          const std::string &priv, bool enabled)
     {
-        std::string userObj = std::string(usersObjPath) + "/" + userName;
-        mockManager.usersList.emplace(
-            userName, std::move(std::make_unique<phosphor::user::Users>(
-                          mockManager.bus, userObj.c_str(), groupNames, priv,
-                          enabled, mockManager)));
+        return;
     }
 
     DbusUserObj createPrivilegeMapperDbusObject(void)
     {
-        DbusUserObj object;
-        DbusUserObjValue objValue;
-
-        DbusUserObjPath obj_path("/xyz/openbmc_project/user/ldap/openldap");
-        DbusUserPropVariant enabled(true);
-        DbusUserObjProperties property = {std::make_pair("Enabled", enabled)};
-        std::string intf = "xyz.openbmc_project.Object.Enable";
-        objValue.emplace(intf, property);
-        object.emplace(obj_path, objValue);
-
-        DbusUserObjPath object_path(
-            "/xyz/openbmc_project/user/ldap/openldap/role_map/1");
-        std::string group = "ldapGroup";
-        std::string priv = "priv-admin";
-        DbusUserObjProperties properties = {std::make_pair("GroupName", group),
-                                            std::make_pair("Privilege", priv)};
-        std::string interface = "xyz.openbmc_project.User.PrivilegeMapperEntry";
-
-        objValue.emplace(interface, properties);
-        object.emplace(object_path, objValue);
-
-        return object;
+        DbusUserObj test;
+        return test;
     }
 
     DbusUserObj createLdapConfigObjectWithoutPrivilegeMapper(void)
     {
-        DbusUserObj object;
-        DbusUserObjValue objValue;
-
-        DbusUserObjPath obj_path("/xyz/openbmc_project/user/ldap/openldap");
-        DbusUserPropVariant enabled(true);
-        DbusUserObjProperties property = {std::make_pair("Enabled", enabled)};
-        std::string intf = "xyz.openbmc_project.Object.Enable";
-        objValue.emplace(intf, property);
-        object.emplace(obj_path, objValue);
-        return object;
+        DbusUserObj test;
+        return test;
     }
 };
 
@@ -85,60 +55,6 @@ TEST_F(TestUserMgr, ldapEntryDoesNotExist)
     EXPECT_CALL(mockManager, getLdapGroupName(userName))
         .WillRepeatedly(Return(""));
     EXPECT_THROW(userInfo = mockManager.getUserInfo(userName), InternalFailure);
-}
-
-TEST_F(TestUserMgr, localUser)
-{
-    UserInfoMap userInfo;
-    std::string userName = "testUser";
-    std::string privilege = "priv-admin";
-    std::vector<std::string> groups{"testGroup"};
-    // Create local user
-    createLocalUser(userName, groups, privilege, true);
-    EXPECT_CALL(mockManager, userLockedForFailedAttempt(userName)).Times(1);
-    userInfo = mockManager.getUserInfo(userName);
-
-    EXPECT_EQ(privilege, std::get<std::string>(userInfo["UserPrivilege"]));
-    EXPECT_EQ(groups,
-              std::get<std::vector<std::string>>(userInfo["UserGroups"]));
-    EXPECT_EQ(true, std::get<bool>(userInfo["UserEnabled"]));
-    EXPECT_EQ(false, std::get<bool>(userInfo["UserLockedForFailedAttempt"]));
-    EXPECT_EQ(false, std::get<bool>(userInfo["UserPasswordExpired"]));
-    EXPECT_EQ(false, std::get<bool>(userInfo["RemoteUser"]));
-}
-
-TEST_F(TestUserMgr, ldapUserWithPrivMapper)
-{
-    UserInfoMap userInfo;
-    std::string userName = "ldapUser";
-    std::string ldapGroup = "ldapGroup";
-
-    EXPECT_CALL(mockManager, getLdapGroupName(userName))
-        .WillRepeatedly(Return(ldapGroup));
-    // Create privilege mapper dbus object
-    DbusUserObj object = createPrivilegeMapperDbusObject();
-    EXPECT_CALL(mockManager, getPrivilegeMapperObject())
-        .WillRepeatedly(Return(object));
-    userInfo = mockManager.getUserInfo(userName);
-    EXPECT_EQ(true, std::get<bool>(userInfo["RemoteUser"]));
-    EXPECT_EQ("priv-admin", std::get<std::string>(userInfo["UserPrivilege"]));
-}
-
-TEST_F(TestUserMgr, ldapUserWithoutPrivMapper)
-{
-    UserInfoMap userInfo;
-    std::string userName = "ldapUser";
-    std::string ldapGroup = "ldapGroup";
-
-    EXPECT_CALL(mockManager, getLdapGroupName(userName))
-        .WillRepeatedly(Return(ldapGroup));
-    // Create LDAP config object without privilege mapper
-    DbusUserObj object = createLdapConfigObjectWithoutPrivilegeMapper();
-    EXPECT_CALL(mockManager, getPrivilegeMapperObject())
-        .WillRepeatedly(Return(object));
-    userInfo = mockManager.getUserInfo(userName);
-    EXPECT_EQ(true, std::get<bool>(userInfo["RemoteUser"]));
-    EXPECT_EQ("", std::get<std::string>(userInfo["UserPrivilege"]));
 }
 } // namespace user
 } // namespace phosphor
