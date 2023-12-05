221f15e489d3b86fa0942eb40a5cd7150f2abfaf
nitinkotania <gitnkotania@gmail.com>
2023-12-05 12:04:59 -0600

    Added support to collect LDAP detailed data.
    LDAP related detailed data will be collected as part
    of BMC dump.
    
    Tested By:
       Tested by collecting BMC dump.
    
    Change-Id: I14d5ef4164983a45333ebfc2320f30d855aa29e9
    Signed-off-by: nitinkotania <gitnkotania@gmail.com>

--- mainapp.cpp
+++ mainapp.cpp
@@ -4,6 +4,16 @@
 
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/manager.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/source/io.hpp>
+#include <sdeventplus/source/signal.hpp>
+#include <stdplus/signal.hpp>
+
+#include <iostream>
+
+using namespace sdeventplus;
+using namespace sdeventplus::source;
+using sdeventplus::source::Signal;
 
 // D-Bus root for user manager
 constexpr auto userManagerRoot = "/xyz/openbmc_project/user";
@@ -18,6 +28,21 @@ int main(int /*argc*/, char** /*argv*/)
     // Claim the bus now
     bus.request_name(USER_MANAGER_BUSNAME);
 
+    auto event = sdeventplus::Event::get_default();
+    bus.attach_event(event.get(), SD_EVENT_PRIORITY_NORMAL);
+    stdplus::signal::block(SIGUSR1);
+
+    sdeventplus::source::Signal sigUsr1(
+        event, SIGUSR1,
+        [&userMgr](sdeventplus::source::Signal& /*signal*/,
+                   const struct signalfd_siginfo*) {
+        std::cout << "LDAP Received SIGUR1(10) Signal interrupt" << std::endl;
+        if (userMgr.isLdapEnabled())
+        {
+            userMgr.ldapDumpCollector();
+        }
+    });
+    event.loop();
     // Wait for client request
     bus.process_loop();
 }
