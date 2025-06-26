dac0035eeea51583df2bdeb3fbdb6c70c3acb312
Patrick Williams <patrick@stwcx.xyz>
2025-06-25 23:24:44 -0400

    user-mgr: add unit test for executeCmd
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I0a7110c12504b547192e186db334e4b62eaaea4e

---
+++ test/execute_cmd_test.cpp
@@ -0,0 +1,39 @@
+#include "user_mgr.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(ExecuteCmdTest, CommandReturnsEmptyOutput)
+{
+    std::vector<std::string> output = phosphor::user::executeCmd("/bin/true");
+    ASSERT_TRUE(output.empty());
+}
+
+TEST(ExecuteCmdTest, CommandWithArgs)
+{
+    std::vector<std::string> output = phosphor::user::executeCmd(
+        "/bin/echo", "testing", "with", "multiple", "args");
+    ASSERT_EQ(output.size(), 1);
+    EXPECT_EQ(output[0], "testing with multiple args");
+}
+
+TEST(ExecuteCmdTest, CommandReturnsOutput)
+{
+    std::vector<std::string> output =
+        phosphor::user::executeCmd("/bin/echo", "-e", "hello\\nworld");
+    ASSERT_EQ(output.size(), 2);
+    EXPECT_EQ(output[0], "hello");
+    EXPECT_EQ(output[1], "world");
+}
+
+TEST(ExecuteCmdTest, NonExistentCommand)
+{
+    EXPECT_THROW(phosphor::user::executeCmd("/path/to/nonexistent_command"),
+                 boost::process::process_error);
+}
+
+TEST(ExecuteCmdTest, CommandReturnsNonZeroExitCode)
+{
+    EXPECT_THROW(
+        phosphor::user::executeCmd("/bin/false"),
+        sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure);
+}
